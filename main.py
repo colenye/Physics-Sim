062576e027ab0d52317727d285d8ee66cf1fa3a5
pestana6 <francisco.pestana@mail.utoronto.ca>
2024-10-21 16:47:58 -0400

    bouncing ball

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import pygame as p
 from sys import exit
 
 p.init()
+
+class Floor:
+    def __init__(self) -> None:
+        self.rect = p.Rect(0, RESOLUTION[1] * 0.8, RESOLUTION[0], RESOLUTION[1] * 0.2)
+    def update(self):
+        p.draw.rect(display, p.Color(0, 0, 0), self.rect)
+
 class Ball:
     def __init__(self, radius=50, x=50, y=50) -> None:
         self.x = 5
@@ -12,12 +19,13 @@ class Ball:
     
     def update(self):
         self.rect.y += self.gravity
+        self.gravity += 0.01
 RESOLUTION = (800, 600)
-GRAVITY = 1
+GRAVITY = 0.1
 balls = []
 display = p.display.set_mode(RESOLUTION)
 
-
+floor = Floor()
 
 while True:
     for event in p.event.get():
@@ -27,10 +35,12 @@ while True:
         if event.type == p.QUIT:
             p.quit()
             exit()
-    print(balls)
     display.fill(p.Color(255, 255, 255))
+    floor.update()
     for ball in balls:
         p.draw.circle(display, p.Color(255, 0, 0), (ball.rect.x, ball.rect.y), ball.radius)
+        if p.Rect.colliderect(ball.rect, floor.rect):
+            ball.gravity = -(ball.gravity * 0.5)
         ball.update()
     
     p.display.update()
